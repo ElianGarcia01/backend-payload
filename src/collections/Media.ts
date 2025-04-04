be20ecc4cd067de8b3d62957e3e62fe3771ce1d2
Elian Garcia <telljua90@gmail.com>
2025-04-04 02:15:44 -0600

    mejora en las dimensiones de imaganes

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,8 +2,46 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  access: {
-    read: () => true,
+  upload: {
+    imageSizes: [
+      {
+        name: 'mobile',
+        width: 150,
+        height: 150,
+        fit: 'inside',
+      },
+      {
+        name: 'thumbnail',
+        width: 320,
+        height: 240,
+        fit: 'inside',
+      },
+      {
+        name: 'medium',
+        width: 640,
+        height: 480,
+        fit: 'inside',
+      },
+      {
+        name: 'large',
+        width: 1280,
+        height: 720,
+        fit: 'inside',
+      },
+      {
+        name: 'banner',
+        width: 1920,
+        height: 1080,
+        fit: 'cover', // Forza recorte 16:9
+        position: 'centre',
+      },
+    ],
+    formatOptions: {
+      format: 'webp',
+      options: { quality: 80 },
+    },
+    // Opcional: Conserva original para compatibilidad
+    disableLocalStorage: false,
   },
   fields: [
     {
@@ -12,5 +50,4 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
 }
